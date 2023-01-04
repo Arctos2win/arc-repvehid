3935ec2cd707b695c72da94be5ae1d946fe586f2
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2023-01-04 01:22:26 +0100

    Added some checks

--- arc-repvehid/server.lua
+++ arc-repvehid/server.lua
@@ -1,13 +1,23 @@
 ESX.RegisterCommand({'repairvehid', 'rvi'}, 'admin',
                     function(xPlayer, args, showError)
-                        local xTarget = ESX.GetPlayerFromId(args.targetid) 
-                        if xTarget == nil then xPlayer.showNotification("De speler die je hebt opgegeven is niet gevonden.") return end
-                        local targetPed = GetPlayerPed(args.targetid) 
-                        local targetVehicle = GetVehiclePedIsIn(targetPed, true) 
-                        local netid = NetworkGetNetworkIdFromEntity(targetVehicle)  
-                        SetNetworkIdExistsOnAllMachines(netid, true)  
-                        TriggerClientEvent("repveh:repair", -1, netid)        
-                    end, false, { help = "repairvehicleid", 
+    local xTarget = ESX.GetPlayerFromId(args.targetid)
+    if xTarget == nil then
+        xPlayer.showNotification("We can't find a player with that player id.")
+        return
+    end
+    
+    local targetPed = GetPlayerPed(args.targetid)
+    local targetVehicle = GetVehiclePedIsIn(targetPed, true)
+    if targetVehicle and DoesEntityExist(targetVehicle) then
+        local netid = NetworkGetNetworkIdFromEntity(targetVehicle)
+        SetNetworkIdExistsOnAllMachines(netid, true)
+        TriggerClientEvent("repveh:repair", -1, netid)
+        xTarget.showNotification("Your vehicle has been fixed !") 
+    else 
+        xPlayer.showNotification("The player doesn't have a vehicle.")
+    end
+end, false, {
+    help = "repairvehicleid",
     arguments = {
         {
             name = 'targetid',
@@ -15,4 +25,9 @@ ESX.RegisterCommand({'repairvehid', 'rvi'}, 'admin',
             type = 'number'
         }
     }
+<<<<<<< HEAD
+})
+=======
 }) 
+
+>>>>>>> 5172dac2694e63c4663e66395eb0966d8e85ca5c
