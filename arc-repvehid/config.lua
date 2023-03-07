e6672d302b29b2971a79c4eba633297435a36056
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2023-03-07 20:34:51 +0100

    forget some information here xD

--- arc-repvehid/config.lua
+++ arc-repvehid/config.lua
@@ -1,5 +1,5 @@
 Config = {} 
 
 Config.Standalone = false --[[ if its standalone it will only use FiveM natives that are documentated here: https://docs.fivem.net/natives/ 
-                             else it will use esx to register the command and notify the player(s) ]] -- 
+                             else it will use esx to register the command and notify the player(s) ]] -- its ESX or Standalone xD
                             
